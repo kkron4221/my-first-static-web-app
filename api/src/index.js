98c7d8dbdc091c9f155c1fe295987c4284eb1c03
ricky <knroro1877887@gmail.com>
2025-07-30 16:54:07 +0900

    Call API

---
+++ api/src/index.js
@@ -0,0 +1,21 @@
+module.exports = async function (context) {
+    context.res.json({
+        time: get_time()
+    });
+};
+
+function get_time(){
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth()+1;
+    var date = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    return (
+        year + "/" + 
+        month + "/" +
+        date + " " +
+        hour + ":" +
+        minute
+    );
+}
